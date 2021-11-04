a63ec32adcf60ee6d7a3b1115eabe149637d73aa
treeform <starplant@gmail.com>
2021-11-04 16:10:02 -0700

    Run both threads on/off tests.

--- tests/test_pool.nim
+++ tests/test_pool.nim
@@ -1,51 +1,54 @@
 import std/os, std/locks, puppy, puppy/pool, osproc
 
-var p = startProcess("tests/debug_server")
-sleep(100)
+when not compileOption("threads"):
+  echo "do nothing maybe pass --threads:on?"
+else:
+  var p = startProcess("tests/debug_server")
+  sleep(100)
 
-openPool()
+  openPool()
 
-var handle1 = fetchParallel(Request(
-  url: parseUrl("http://localhost:8080/ok"),
-  verb: "get"
-))
+  var handle1 = fetchParallel(Request(
+    url: parseUrl("http://localhost:8080/ok"),
+    verb: "get"
+  ))
 
-var handle2 = fetchParallel(Request(
-  url: parseUrl("http://localhost:8080/slow"),
-  verb: "get"
-))
+  var handle2 = fetchParallel(Request(
+    url: parseUrl("http://localhost:8080/slow"),
+    verb: "get"
+  ))
 
-var handle3 = fetchParallel(Request(
-  url: parseUrl("http://localhost:8080/404"),
-  verb: "get"
-))
+  var handle3 = fetchParallel(Request(
+    url: parseUrl("http://localhost:8080/404"),
+    verb: "get"
+  ))
 
-while true:
-  if handle1 != -1 and handle1.ready:
-    let r = handle1.response
-    echo "res.code:", r.code
-    echo "res.headers:", r.headers
-    echo "res.body:", r.body
-    handle1 = -1
+  while true:
+    if handle1 != -1 and handle1.ready:
+      let r = handle1.response
+      echo "res.code:", r.code
+      echo "res.headers:", r.headers
+      echo "res.body:", r.body
+      handle1 = -1
 
-  if handle2 != -1 and handle2.ready:
-    let r = handle2.response
-    echo "res.code:", r.code
-    echo "res.headers:", r.headers
-    echo "res.body:", r.body
-    handle2 = -1
+    if handle2 != -1 and handle2.ready:
+      let r = handle2.response
+      echo "res.code:", r.code
+      echo "res.headers:", r.headers
+      echo "res.body:", r.body
+      handle2 = -1
 
-  if handle3 != -1 and handle3.ready:
-    let r = handle3.response
-    echo "res.code:", r.code
-    echo "res.headers:", r.headers
-    echo "res.body:", r.body.len
-    handle3 = -1
+    if handle3 != -1 and handle3.ready:
+      let r = handle3.response
+      echo "res.code:", r.code
+      echo "res.headers:", r.headers
+      echo "res.body:", r.body.len
+      handle3 = -1
 
-  if handle1 == -1 and handle2 == -1 and handle3 == -1:
-    break
+    if handle1 == -1 and handle2 == -1 and handle3 == -1:
+      break
 
-  sleep(1)
+    sleep(1)
 
-closePool()
-p.terminate()
+  closePool()
+  p.terminate()
--- tests/test_pool_stress.nim
+++ tests/test_pool_stress.nim
@@ -1,38 +1,42 @@
-import std/os, std/locks, puppy, puppy/pool, osproc, streams
 
-var p = startProcess("tests/debug_server", options={poParentStreams})
-sleep(100)
+when not compileOption("threads"):
+  echo "do nothing maybe pass --threads:on?"
+else:
+  import std/os, std/locks, puppy, puppy/pool, osproc, streams
 
-openPool()
+  var p = startProcess("tests/debug_server", options={poParentStreams})
+  sleep(100)
 
-var handles: seq[RequestHandle]
-for i in 0 ..< 1000:
-  handles.add fetchParallel(Request(
-    url: parseUrl("http://localhost:8080/page?ret=" & $i),
-    verb: "get"
-  ))
+  openPool()
 
-while true:
-  var running = 0
-  var change = false
-  for id, handle in handles:
-    if handle != -1 and handle.ready:
-      let r = handle.response
-      doAssert r.code == 200
-      doAssert r.headers.len == 1
-      doAssert r.body == $id
-      handles[id] = -1
-      change = true
-    if handle != -1:
-      inc running
+  var handles: seq[RequestHandle]
+  for i in 0 ..< 1000:
+    handles.add fetchParallel(Request(
+      url: parseUrl("http://localhost:8080/page?ret=" & $i),
+      verb: "get"
+    ))
 
-  if change:
-    echo "running: ", running, "/", handles.len
-  if running == 0:
-    break
+  while true:
+    var running = 0
+    var change = false
+    for id, handle in handles:
+      if handle != -1 and handle.ready:
+        let r = handle.response
+        doAssert r.code == 200
+        doAssert r.headers.len == 1
+        doAssert r.body == $id
+        handles[id] = -1
+        change = true
+      if handle != -1:
+        inc running
 
-  sleep(1)
+    if change:
+      echo "running: ", running, "/", handles.len
+    if running == 0:
+      break
 
-closePool()
-p.terminate()
-sleep(100)
+    sleep(1)
+
+  closePool()
+  p.terminate()
+  sleep(100)
