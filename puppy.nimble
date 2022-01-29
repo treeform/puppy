dac5f09ff75a75d862b321dc581219f68a545437
Ryan Oldenburg <ryan@guzba.com>
2022-01-28 19:55:44 -0600

    lower nim version dep, add nim version to github actions

--- puppy.nimble
+++ puppy.nimble
@@ -1,11 +1,11 @@
-version     = "1.5.2"
+version     = "1.5.3"
 author      = "Andre von Houck"
 description = "Puppy fetches HTML pages for Nim."
 license     = "MIT"
 
 srcDir = "src"
 
-requires "nim >= 1.4.4"
+requires "nim >= 1.2.2"
 requires "urlly >= 0.2.0"
 requires "libcurl >= 1.0.0"
 requires "zippy >= 0.7.3"
--- tests/test_issue48.nim
+++ tests/test_issue48.nim
@@ -1,67 +1,68 @@
-when not compileOption("threads"):
-  echo "This test requres --threads:on"
-else:
-  import puppy
+when (NimMajor, NimMinor, NimPatch) >= (1, 6, 0):
+  when not compileOption("threads"):
+    echo "This test requres --threads:on"
+  else:
+    import puppy
 
-  type
-    ChannelRef*[T] = ptr Channel[T]
-    Client* = ref object
-      requestThread*: Thread[Client]
-      action*: ChannelRef[Action]
-    ActionKind* = enum
-      Stop, Fetch,
-    Action* = object
-      case kind*: ActionKind
-      of Stop:
-        discard
-      of Fetch:
-        request*: Request
+    type
+      ChannelRef*[T] = ptr Channel[T]
+      Client* = ref object
+        requestThread*: Thread[Client]
+        action*: ChannelRef[Action]
+      ActionKind* = enum
+        Stop, Fetch,
+      Action* = object
+        case kind*: ActionKind
+        of Stop:
+          discard
+        of Fetch:
+          request*: Request
 
-  proc sendAction*(client: Client, action: Action) =
-    client.action[].send(action)
+    proc sendAction*(client: Client, action: Action) =
+      client.action[].send(action)
 
-  proc recvAction(client: Client) {.thread.} =
-    while true:
-      let action = client.action[].recv()
-      case action.kind:
-      of Stop:
-        break
-      of Fetch:
-        discard fetch(action.request)
-        echo "Fetch success!"
+    proc recvAction(client: Client) {.thread.} =
+      while true:
+        let action = client.action[].recv()
+        case action.kind:
+        of Stop:
+          break
+        of Fetch:
+          discard fetch(action.request)
+          echo "Fetch success!"
 
-  proc initShared(client: var Client) =
-    client.action = cast[ChannelRef[Action]](
-      allocShared0(sizeof(Channel[Action]))
-    )
-    client.action[].open()
+    proc initShared(client: var Client) =
+      client.action = cast[ChannelRef[Action]](
+        allocShared0(sizeof(Channel[Action]))
+      )
+      client.action[].open()
 
-  proc deinitShared(client: var Client) =
-    client.action[].close()
-    deallocShared(client.action)
+    proc deinitShared(client: var Client) =
+      client.action[].close()
+      deallocShared(client.action)
 
-  proc initThreads(client: var Client) =
-    createThread(client.requestThread, recvAction, client)
+    proc initThreads(client: var Client) =
+      createThread(client.requestThread, recvAction, client)
 
-  proc deinitThreads(client: var Client) =
-    client.action[].send(Action(kind: Stop))
-    client.requestThread.joinThread() # on puppy 1.4.0, it gets stuck here
+    proc deinitThreads(client: var Client) =
+      client.action[].send(Action(kind: Stop))
+      client.requestThread.joinThread() # on puppy 1.4.0, it gets stuck here
 
-  proc start*(client: var Client) =
-    initShared(client)
-    initThreads(client)
+    proc start*(client: var Client) =
+      initShared(client)
+      initThreads(client)
 
-  proc stop*(client: var Client) =
-    deinitThreads(client)
-    deinitShared(client)
-    echo "Stopped client thread"
+    proc stop*(client: var Client) =
+      deinitThreads(client)
+      deinitShared(client)
+      echo "Stopped client thread"
 
-  when isMainModule:
-    var client = Client()
-    client.start()
-    let req = Request(
-      url: parseUrl("https://nim-lang.org/"),
-      verb: "get",
-    )
-    client.sendAction(Action(kind: Fetch, request: req))
-    client.stop()
+    when isMainModule:
+      var client = Client()
+      client.start()
+      let req = Request(
+        url: parseUrl("https://nim-lang.org/"),
+        verb: "get",
+      )
+      client.sendAction(Action(kind: Fetch, request: req))
+      client.stop()
