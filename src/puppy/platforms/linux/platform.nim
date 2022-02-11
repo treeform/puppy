7af81b5392e8f1e0d7f37a39d61b806f5c7122f0
Ryan Oldenburg <ryan@guzba.com>
2022-02-11 01:12:27 -0600

    platforms

---
+++ src/puppy/platforms/linux/platform.nim
@@ -0,0 +1,94 @@
+import libcurl, puppy/common, std/strutils, zippy
+
+type StringWrap = object
+  ## As strings are value objects they need
+  ## some sort of wrapper to be passed to C.
+  str: string
+
+proc fetch*(req: Request): Response {.raises: [PuppyError].} =
+  result = Response()
+
+  proc curlWriteFn(
+    buffer: cstring,
+    size: int,
+    count: int,
+    outstream: pointer
+  ): int {.cdecl.} =
+    if size != 1:
+      raise newException(PuppyError, "Unexpected curl write callback size")
+    let
+      outbuf = cast[ptr StringWrap](outstream)
+      i = outbuf.str.len
+    outbuf.str.setLen(outbuf.str.len + count)
+    copyMem(outbuf.str[i].addr, buffer, count)
+    result = size * count
+
+  var strings: seq[string]
+  strings.add $req.url
+  strings.add req.verb.toUpperAscii()
+  for header in req.headers:
+    strings.add header.key & ": " & header.value
+
+  let curl = easy_init()
+
+  discard curl.easy_setopt(OPT_URL, strings[0].cstring)
+  discard curl.easy_setopt(OPT_CUSTOMREQUEST, strings[1].cstring)
+  discard curl.easy_setopt(OPT_TIMEOUT, req.timeout.int)
+
+  # Create the Pslist for passing headers to curl manually. This is to
+  # avoid needing to call slist_free_all which creates problems
+  var slists: seq[Slist]
+  for i, header in req.headers:
+    slists.add Slist(data: strings[2 + i].cstring, next: nil)
+  # Do this in two passes so the slists index addresses are stable
+  var headerList: Pslist
+  for i, header in req.headers:
+    if i == 0:
+      headerList = slists[0].addr
+    else:
+      var tail = headerList
+      while tail.next != nil:
+        tail = tail.next
+      tail.next = slists[i].addr
+
+  discard curl.easy_setopt(OPT_HTTPHEADER, headerList)
+
+  if req.verb.toUpperAscii() == "POST" or req.body.len > 0:
+    discard curl.easy_setopt(OPT_POSTFIELDSIZE, req.body.len)
+    discard curl.easy_setopt(OPT_POSTFIELDS, req.body.cstring)
+
+  # Setup writers.
+  var headerWrap, bodyWrap: StringWrap
+  discard curl.easy_setopt(OPT_WRITEDATA, bodyWrap.addr)
+  discard curl.easy_setopt(OPT_WRITEFUNCTION, curlWriteFn)
+  discard curl.easy_setopt(OPT_HEADERDATA, headerWrap.addr)
+  discard curl.easy_setopt(OPT_HEADERFUNCTION, curlWriteFn)
+
+  # On windows look for cacert.pem.
+  when defined(windows):
+    discard curl.easy_setopt(OPT_CAINFO, "cacert.pem".cstring)
+  # Follow redirects by default.
+  discard curl.easy_setopt(OPT_FOLLOWLOCATION, 1)
+
+  let
+    ret = curl.easy_perform()
+    headerData = headerWrap.str
+
+  curl.easy_cleanup()
+
+  if ret == E_OK:
+    var httpCode: uint32
+    discard curl.easy_getinfo(INFO_RESPONSE_CODE, httpCode.addr)
+    result.code = httpCode.int
+    for headerLine in headerData.split(CRLF):
+      let arr = headerLine.split(":", 1)
+      if arr.len == 2:
+        result.headers.add(Header(key: arr[0].strip(), value: arr[1].strip()))
+    result.body = bodyWrap.str
+    if result.headers["Content-Encoding"] == "gzip":
+      try:
+        result.body = uncompress(result.body, dfGzip)
+      except ZippyError as e:
+        raise newException(PuppyError, "Error uncompressing response", e)
+  else:
+    raise newException(PuppyError, $easy_strerror(ret))
---
+++ src/puppy/platforms/win32/platform.nim
@@ -0,0 +1,224 @@
+import puppy/common, std/strutils, utils, windefs, zippy
+
+proc fetch*(req: Request): Response {.raises: [PuppyError].} =
+  result = Response()
+
+  var hSession, hConnect, hRequest: HINTERNET
+  try:
+    let wideUserAgent = req.headers["user-agent"].wstr()
+
+    hSession = WinHttpOpen(
+      cast[ptr WCHAR](wideUserAgent[0].unsafeAddr),
+      WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY,
+      nil,
+      nil,
+      0
+    )
+    if hSession == nil:
+      raise newException(
+        PuppyError, "WinHttpOpen error: " & $GetLastError()
+      )
+
+    let ms = (req.timeout * 1000).int32
+    if WinHttpSetTimeouts(hSession, ms, ms, ms, ms) == 0:
+      raise newException(
+        PuppyError, "WinHttpSetTimeouts error: " & $GetLastError()
+      )
+
+    var port: INTERNET_PORT
+    if req.url.port == "":
+      case req.url.scheme:
+      of "http":
+        port = 80
+      of "https":
+        port = 443
+      else:
+        discard # Scheme is validated above
+    else:
+      try:
+        let parsedPort = parseInt(req.url.port)
+        if parsedPort < 0 or parsedPort > uint16.high.int:
+          raise newException(PuppyError, "Invalid port: " & req.url.port)
+        port = parsedPort.uint16
+      except ValueError as e:
+        raise newException(PuppyError, "Parsing port failed", e)
+
+    let wideHostname = req.url.hostname.wstr()
+
+    hConnect = WinHttpConnect(
+      hSession,
+      cast[ptr WCHAR](wideHostname[0].unsafeAddr),
+      port,
+      0
+    )
+    if hConnect == nil:
+      raise newException(
+        PuppyError, "WinHttpConnect error: " & $GetLastError()
+      )
+
+    var openRequestFlags: DWORD
+    if req.url.scheme == "https":
+      openRequestFlags = openRequestFlags or WINHTTP_FLAG_SECURE
+
+    var objectName = req.url.path
+    if req.url.search != "":
+      objectName &= "?" & req.url.search
+
+    let
+      wideVerb = req.verb.toUpperAscii().wstr()
+      wideObjectName = objectName.wstr()
+
+    let
+      defaultAcceptType = "*/*".wstr()
+      defaultacceptTypes = [
+        cast[ptr WCHAR](defaultAcceptType[0].unsafeAddr),
+        nil
+      ]
+
+    hRequest = WinHttpOpenRequest(
+      hConnect,
+      cast[ptr WCHAR](wideVerb[0].unsafeAddr),
+      cast[ptr WCHAR](wideObjectName[0].unsafeAddr),
+      nil,
+      nil,
+      cast[ptr ptr WCHAR](defaultacceptTypes.unsafeAddr),
+      openRequestFlags.DWORD
+    )
+    if hRequest == nil:
+      raise newException(
+        PuppyError, "WinHttpOpenRequest error: " & $GetLastError()
+      )
+
+    var requestHeaderBuf: string
+    for header in req.headers:
+      requestHeaderBuf &= header.key & ": " & header.value & CRLF
+
+    let wideRequestHeaderBuf = requestHeaderBuf.wstr()
+
+    if WinHttpAddRequestHeaders(
+      hRequest,
+      cast[ptr WCHAR](wideRequestHeaderBuf[0].unsafeAddr),
+      -1,
+      (WINHTTP_ADDREQ_FLAG_ADD or WINHTTP_ADDREQ_FLAG_REPLACE).DWORD
+    ) == 0:
+      raise newException(
+        PuppyError, "WinHttpAddRequestHeaders error: " & $GetLastError()
+      )
+
+    if WinHttpSendRequest(
+      hRequest,
+      nil,
+      0,
+      req.body.cstring,
+      req.body.len.DWORD,
+      req.body.len.DWORD,
+      0
+    ) == 0:
+      raise newException(
+        PuppyError, "WinHttpSendRequest error: " & $GetLastError()
+      )
+
+    if WinHttpReceiveResponse(hRequest, nil) == 0:
+      raise newException(
+        PuppyError, "WinHttpReceiveResponse error: " & $GetLastError()
+      )
+
+    var
+      statusCode: DWORD
+      dwSize = sizeof(DWORD).DWORD
+    if WinHttpQueryHeaders(
+      hRequest,
+      WINHTTP_QUERY_STATUS_CODE or WINHTTP_QUERY_FLAG_NUMBER,
+      nil,
+      statusCode.addr,
+      dwSize.addr,
+      nil
+    ) == 0:
+      raise newException(
+        PuppyError, "WinHttpQueryHeaders error: " & $GetLastError()
+      )
+
+    result.code = statusCode
+
+    var
+      responseHeaderBytes: DWORD
+      responseHeaderBuf: string
+
+    # Determine how big the header buffer needs to be
+    discard WinHttpQueryHeaders(
+      hRequest,
+      WINHTTP_QUERY_RAW_HEADERS_CRLF,
+      nil,
+      nil,
+      responseHeaderBytes.addr,
+      nil
+    )
+    let errorCode = GetLastError()
+    if errorCode == ERROR_INSUFFICIENT_BUFFER: # Expected!
+      # Set the header buffer to the correct size and inclue a null terminator
+      responseHeaderBuf.setLen(responseHeaderBytes)
+    else:
+      raise newException(PuppyError, "HttpQueryInfoW error: " & $errorCode)
+
+    # Read the headers into the buffer
+    if WinHttpQueryHeaders(
+      hRequest,
+      WINHTTP_QUERY_RAW_HEADERS_CRLF,
+      nil,
+      responseHeaderBuf[0].addr,
+      responseHeaderBytes.addr,
+      nil
+    ) == 0:
+      raise newException(PuppyError, "HttpQueryInfoW error: " & $errorCode)
+
+    let responseHeaders =
+      ($cast[ptr WCHAR](responseHeaderBuf[0].addr)).split(CRLF)
+
+    if responseHeaders.len == 0:
+      raise newException(PuppyError, "Error parsing response headers")
+
+    for i, line in responseHeaders:
+      if i == 0: # HTTP/1.1 200 OK
+        continue
+      if line != "":
+        let parts = line.split(":", 1)
+        if parts.len == 2:
+          result.headers.add(Header(
+            key: parts[0].strip(),
+            value: parts[1].strip()
+          ))
+
+    var i: int
+    result.body.setLen(8192)
+
+    while true:
+      var bytesRead: DWORD
+      if WinHttpReadData(
+        hRequest,
+        result.body[i].addr,
+        (result.body.len - i).DWORD,
+        bytesRead.addr
+      ) == 0:
+        raise newException(
+          PuppyError, "WinHttpReadData error: " & $GetLastError()
+        )
+
+      i += bytesRead
+
+      if bytesRead == 0:
+        break
+
+      if i == result.body.len:
+        result.body.setLen(min(i * 2, i + 100 * 1024 * 1024))
+
+    result.body.setLen(i)
+
+    if result.headers["content-encoding"].toLowerAscii() == "gzip":
+      try:
+        result.body = uncompress(result.body, dfGzip)
+      except ZippyError as e:
+        raise newException(PuppyError, "Error uncompressing response", e)
+  finally:
+    discard WinHttpCloseHandle(hRequest)
+    discard WinHttpCloseHandle(hConnect)
+    discard WinHttpCloseHandle(hSession)
